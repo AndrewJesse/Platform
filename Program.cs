8a4523dc0c6e156f8c93419496ef257e2ab0ea9c
A.J. Marchese <aj.marchese@outlook.com>
2023-03-28 08:43:34 -0700

    removed comments

--- Program.cs
+++ Program.cs
@@ -8,20 +8,13 @@ app.Use(async (context, next) =>
     WriteAsync($"\nStatus Code: {context.Response.StatusCode}"
         ); 
 });
-// Define a middleware function that takes in an HTTP context and a next function
 app.Use(async (context, next) =>
 {
-    // Check if the request method is GET and if the "custom" query parameter is set to "true"
     if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
     {
-        // Set the content type of the response to "text/plain"
         context.Response.ContentType = "text/plain";
-
-        // Write a message to the response body
         await context.Response.WriteAsync("Custom Middleware \n");
     }
-
-    // Call the next middleware in the pipeline
     await next();
 }
 );

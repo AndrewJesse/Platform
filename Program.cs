5ae9887f63373d0b3b68ebaddd4f9b1cf630aedf
A.J. Marchese <aj.marchese@outlook.com>
2023-03-31 08:48:46 -0700

    added different messages based on the response

--- Program.cs
+++ Program.cs
@@ -9,16 +9,28 @@ builder.Services.Configure<RouteOptions>(opts => {
 
 var app = builder.Build();
 
-app.Map("{number:int}", async context => { 
-    await context.Response.WriteAsync("Routed to the int endpoint");
-}).Add(b => ((RouteEndpointBuilder)b).Order = 1); 
+app.Use(async (context, next) => { 
+    Endpoint? end = context.GetEndpoint(); 
+    if (end != null) { 
+        await context.Response
+        .WriteAsync($"{end.DisplayName} Selected \n"); 
+    } else { 
+        await context.Response.WriteAsync("No Endpoint Selected \n"); 
+    }
+    await next();
+}); 
+app.Map("{number:int}", async context => {
+    await context.Response.WriteAsync("Routed to the int endpoint"); 
+}).WithDisplayName("Int Endpoint")
+.Add(b => ((RouteEndpointBuilder)b).Order = 1); 
 
-app.Map("{number:double}", async context => { 
-    await context.Response.WriteAsync("Routed to the double endpoint");
-}).Add(b => ((RouteEndpointBuilder)b).Order = 2);
+app.Map("{number:double}", async context => {
+    await context.Response.WriteAsync("Routed to the double endpoint"); 
+}).WithDisplayName("Double Endpoint")
+.Add(b => ((RouteEndpointBuilder)b).Order = 2); 
 
 app.MapFallback(async context => { 
-    await context.Response.WriteAsync("404 Not Found. Requested URL was not found on this server."); 
+    await context.Response.WriteAsync("Routed to fallback endpoint"); 
 });
 
 app.Run();

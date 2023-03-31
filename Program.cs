ca3e893d47e0779c2a2c3857a725aada53345ece
A.J. Marchese <aj.marchese@outlook.com>
2023-03-31 08:16:37 -0700

    defined fall back route

--- Program.cs
+++ Program.cs
@@ -15,4 +15,6 @@ app.MapGet("{first:alpha:length(3)}/{second:bool}", async context => {
 app.MapGet("capital/{country:regex(^uk|france|monaco$)}", Capital.Endpoint); 
 app.MapGet("size/{city?}", Population.Endpoint).WithMetadata(new RouteNameMetadata("population"));
 
+app.MapFallback(async context => { await context.Response.WriteAsync("404. That’s an error.\r\n\r\nThe requested URL /ok was not found on this server. That’s all we know."); });
+
 app.Run();

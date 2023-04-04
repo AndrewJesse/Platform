f6e0c19e53727f22c8ee26d399fcfb352abeeebc
A.J. Marchese <aj.marchese@outlook.com>
2023-04-04 08:39:41 -0700

    revised endpoint

--- WeatherEndpoint.cs
+++ WeatherEndpoint.cs
@@ -4,6 +4,11 @@ namespace Platform
 {
     public class WeatherEndpoint
     {
+        private IResponseFormatter formatter;
+        public WeatherEndpoint(IResponseFormatter responseFormatter)
+        {
+            formatter = responseFormatter;
+        }
         public static async Task Endpoint(HttpContext context, IResponseFormatter formatter) 
         {
             await formatter.Format(context, "Endpoint Class: It is cloudy in Milan"); 

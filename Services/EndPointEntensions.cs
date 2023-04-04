b765b3d0d9ab25126e726f5097d2b9fcea7c4439
A.J. Marchese <aj.marchese@outlook.com>
2023-04-04 08:32:53 -0700

    added extension method for IEndPoint

---
+++ Services/EndPointEntensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Platform.Services
+{
+    public static class EndpointExtensions
+    {
+        public static void MapWeather(this IEndpointRouteBuilder app, string path)
+        {
+            IResponseFormatter formatter = app.ServiceProvider.GetRequiredService<IResponseFormatter>();
+            app.MapGet(path, context => Platform.WeatherEndpoint.Endpoint(context, formatter));
+        }
+    }
+}

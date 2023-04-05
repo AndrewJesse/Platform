33157f913116e05b1476a534b03c48d672062380
A.J. Marchese <aj.marchese@outlook.com>
2023-04-05 08:53:20 -0700

    added scoped service to program

--- Program.cs
+++ Program.cs
@@ -16,8 +16,11 @@ app.MapGet("middleware/function", async (HttpContext context, IResponseFormatter
 //app.MapWeather("endpoint/class");
 //app.MapEndpoint<WeatherEndpoint>("endpoint/class");
 
-app.MapGet("endpoint/function", async (HttpContext context, IResponseFormatter formatter) 
-    => { await formatter.Format(context, "Endpoint Function: It is sunny in LA");});
+app.MapGet("endpoint/function", async (HttpContext context) => {
+    IResponseFormatter formatter = 
+    context.RequestServices.GetRequiredService<IResponseFormatter>(); 
+    await formatter.Format(context, "Endpoint Function: It is sunny in LA"); 
+});
 
 app.MapGet("test", async (HttpContext context) => {
     await context.Response.WriteAsync("Hello from the test route!");

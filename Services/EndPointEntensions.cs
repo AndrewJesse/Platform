23a9be1a6169a9d58d93d665b188dd3f0794fe4a
A.J. Marchese <aj.marchese@outlook.com>
2023-04-05 08:50:32 -0700

    intantiated endpoints

--- Services/EndPointEntensions.cs
+++ Services/EndPointEntensions.cs
@@ -13,12 +13,16 @@ namespace Microsoft.AspNetCore.Builder
             T endpointInstance = 
                 ActivatorUtilities.CreateInstance<T>(app.ServiceProvider); 
 
-            ParameterInfo[] methodParams = methodInfo!.GetParameters(); 
+            ParameterInfo[] methodParams = methodInfo!.GetParameters();
 
-            app.MapGet(path, context => (Task)(methodInfo.Invoke(endpointInstance, 
-                methodParams.Select(p => p.ParameterType == typeof(HttpContext) 
-                ? context : 
-            context.RequestServices.GetService(p.ParameterType)).ToArray()))!); 
+            app.MapGet(path, context => {
+                T endpointInstance = 
+                ActivatorUtilities.CreateInstance<T>(context.RequestServices); 
+                return (Task)methodInfo.Invoke(endpointInstance!, 
+                    methodParams.Select(p => p.ParameterType == typeof(HttpContext) 
+                    ? context
+                    : context.RequestServices.GetService(p.ParameterType)).ToArray())!;
+            });
         }
     }
 }

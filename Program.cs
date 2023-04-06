5a52576c3d08b43df13997fe1215c0787c99aa00
A.J. Marchese <aj.marchese@outlook.com>
2023-04-06 07:47:27 -0700

    added config mapping to program

--- Program.cs
+++ Program.cs
@@ -1,29 +1,14 @@
-using Platform;
 using Platform.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<ITimeStamper, DefaultTimeStamper>();
+var app = builder.Build();
+app.MapGet("/",
+    async context => { await context.Response.WriteAsync("Hello World!"); });
 
-builder.Services.AddScoped<IResponseFormatter, GuidService>();
-
-var app = builder.Build(); 
-
-app.UseMiddleware<WeatherMiddleware>();
-
-app.MapGet("middleware/function", async (HttpContext context, IResponseFormatter formatter) 
-    => { await formatter.Format(context, "Middleware Function: It is snowing in Chicago");});
-
-
-//app.MapWeather("endpoint/class");
-//app.MapEndpoint<WeatherEndpoint>("endpoint/class");
-
-app.MapGet("endpoint/function", async (HttpContext context) => {
-    IResponseFormatter formatter = 
-    context.RequestServices.GetRequiredService<IResponseFormatter>(); 
-    await formatter.Format(context, "Endpoint Function: It is sunny in LA"); 
+app.MapGet("config", async (HttpContext ContextBoundObject, IConfiguration config) =>
+{
+    string defaultDebug = config["Logging:LogLevel:Default"];
+    await ContextBoundObject.Response.WriteAsync($"The config setting is: {defaultDebug}");
 });
-
-app.MapGet("test", async (HttpContext context) => {
-    await context.Response.WriteAsync("Hello from the test route!");
-});
-
 app.Run();

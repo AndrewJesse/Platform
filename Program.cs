6910694f7e2d320c572d9fee1d0f01f27bbeb1ce
A.J. Marchese <aj.marchese@outlook.com>
2023-04-06 07:58:37 -0700

    renamed HttpsContext defination

--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@ var app = builder.Build();
 app.MapGet("/",
     async context => { await context.Response.WriteAsync("Hello World!"); });
 
-app.MapGet("config", async (HttpContext ContextBoundObject, IConfiguration config) =>
+app.MapGet("config", async (HttpContext context, IConfiguration config) =>
 {
     string defaultDebug = config["Logging:LogLevel:Default"];
-    await ContextBoundObject.Response.WriteAsync($"The config setting is: {defaultDebug}");
+    await context.Response.WriteAsync($"The config setting is: {defaultDebug}");
 });
 app.Run();

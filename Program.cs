6b7440daaa4c4bee4f7fefdba6c0e6efb5792940
A.J. Marchese <aj.marchese@outlook.com>
2023-04-07 08:59:11 -0700

    created logger in program

--- Program.cs
+++ Program.cs
@@ -2,7 +2,10 @@ using Platform;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-app.Logger.LogDebug("Pipeline configuration starting");
+
+var logger = app.Services.GetRequiredService<ILoggerFactory>().CreateLogger("Pipeline");
+logger.LogDebug("Pipeline configuration starting");
 app.MapGet("population/{city?}", Population.Endpoint);
-app.Logger.LogDebug("Pipeline configuration complete");
+logger.LogDebug("Pipeline configuration complete");
+
 app.Run();

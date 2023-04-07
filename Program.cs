0d390db569a3e3469348e05955aa2fc5a2239809
A.J. Marchese <aj.marchese@outlook.com>
2023-04-07 08:56:47 -0700

    added logging to program

--- Program.cs
+++ Program.cs
@@ -2,5 +2,7 @@ using Platform;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+app.Logger.LogDebug("Pipeline configuration starting");
 app.MapGet("population/{city?}", Population.Endpoint);
+app.Logger.LogDebug("Pipeline configuration complete");
 app.Run();

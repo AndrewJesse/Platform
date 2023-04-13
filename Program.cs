d87988e5adc658e1e06e4f999b456ded966a9a6e
A.J. Marchese <aj.marchese@outlook.com>
2023-04-13 08:30:55 -0700

    return an HTML error response instead

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/error.html");
+    app.UseStaticFiles();
+}
+
 app.Run(context => { throw new Exception("Something has gone wrong"); });
 app.Run();

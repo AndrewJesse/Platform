88f63b4c9a0d38b1a3564d35d0f32d29cc472794
A.J. Marchese <aj.marchese@outlook.com>
2023-04-14 08:53:51 -0700

    added persistant data cache builder to program

--- Program.cs
+++ Program.cs
@@ -1,5 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDistributedMemoryCache(opts => { opts.SizeLimit = 200; });
+//builder.Services.AddDistributedMemoryCache(opts => { opts.SizeLimit = 200; });
+builder.Services.AddDistributedSqlServerCache(opts =>
+{
+    opts.ConnectionString = builder.Configuration["ConnectionStrings:CacheConnection"];
+    opts.SchemaName = "dbo";
+    opts.TableName = "DataCache";
+});
 var app = builder.Build();
 app.MapEndpoint<Platform.SumEndpoint>("/sum/{count:int=1000000000}");
 app.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });

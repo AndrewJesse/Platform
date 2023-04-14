8cd17332a3ee6eb4e3bad68439077c3e16255b90
A.J. Marchese <aj.marchese@outlook.com>
2023-04-14 09:08:05 -0700

    added services to program

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
+using Platform.Services;
 var builder = WebApplication.CreateBuilder(args);
-//builder.Services.AddDistributedMemoryCache(opts => { opts.SizeLimit = 200; });
 builder.Services.AddDistributedSqlServerCache(opts =>
 {
     opts.ConnectionString = builder.Configuration["ConnectionStrings:CacheConnection"];
     opts.SchemaName = "dbo";
     opts.TableName = "DataCache";
 });
+builder.Services.AddResponseCaching();
+builder.Services.AddSingleton<IResponseFormatter, HtmlResponseFormatter>();
 var app = builder.Build();
+app.UseResponseCaching();
 app.MapEndpoint<Platform.SumEndpoint>("/sum/{count:int=1000000000}");
 app.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
 app.Run();

566fe5f856d69697f3be1ceea70ac0fc008d1e1b
A.J. Marchese <aj.marchese@outlook.com>
2023-04-15 12:22:43 -0700

    enabled database seeding

--- Program.cs
+++ Program.cs
@@ -15,8 +15,19 @@ builder.Services.AddDbContext<CalculationContext>(opts =>
 {
     opts.UseSqlServer(builder.Configuration["ConnectionStrings:CalcConnection"]);
 });
+builder.Services.AddTransient<SeedData>();
 var app = builder.Build();
 app.UseResponseCaching();
 app.MapEndpoint<Platform.SumEndpoint>("/sum/{count:int=1000000000}");
 app.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
-app.Run();
+bool cmdLineInit = (app.Configuration["INITDB"] ?? "false") == "true";
+if (app.Environment.IsDevelopment() || cmdLineInit)
+{
+    var seedData = app.Services.GetRequiredService<SeedData>();
+    seedData.SeedDatabase();
+}
+
+if (!cmdLineInit)
+{
+    app.Run();
+}
